bfc66badfb65fa0468568a02cabb027c70b6b02e
Azamor <52185981+azamor-luccas@users.noreply.github.com>
2021-11-28 07:28:10 -0300

    Create remove method

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -67,7 +67,72 @@ class BinarySearchTree {
     return null;
   }
 
-  remove(value){}
+  lookupWithParent(value){
+    let currentNode = this.root;
+    let previousNode = null;
+    while(currentNode) {
+      if (value > currentNode.value){
+        previousNode = currentNode;
+        currentNode = currentNode.right;
+        continue
+
+      } else if (value < currentNode.value){
+        previousNode = currentNode;
+        currentNode = currentNode.left;
+        continue
+      
+      } else {
+        return [currentNode, previousNode];
+      }
+    }
+    return [null, null];
+  }
+
+  remove(value){
+    let [node, parent] = this.lookupWithParent(value);
+    if (!node) throw "Value not found"
+
+    //if it's a leaf
+    if (node.left === null && node.right === null) {
+      if (parent === null) {this.root = null; return}
+      if (parent.left.value === value) parent.left = null;
+      if (parent.right.value === value) parent.right = null;
+    }
+    //if it has only one child
+    else if (node.left === null || node.right === null) {
+      let child = node.left ?? node.right
+
+      if (parent === null) {
+        this.root = child;
+        return
+      }
+
+      if (parent.left.value === value) {
+        parent.left = child;
+      } else {
+        parent.right = child;
+      }
+    }
+    //if it has two children
+    else {
+      let currentNode = node.right;
+      if (currentNode.left === null) {
+        node.value = node.right.value;
+        node.right = currentNode.right;
+        return
+      }
+
+      while(currentNode.left.left) {
+        currentNode = currentNode.left;
+      }
+
+      let successorParentNode = currentNode;
+      let successorNode = currentNode.left;
+
+      node.value = successorNode.value;
+      successorParentNode.left = successorNode.right;
+    }
+  }
 }
 
 myBST = new BinarySearchTree()
@@ -75,10 +140,10 @@ myBST.insert(10)
 myBST.insert(20)
 myBST.insert(15)
 myBST.insert(30)
+
 myBST.insert("04")
 myBST.insert("08")
 myBST.insert("02")
-
 
 let val = myBST.lookup(4);
 console.log(val)
@@ -89,11 +154,15 @@ console.log(val)
 // }
 
 printPrettyTree(myBST.root);
+myBST.remove(10)
+
+printPrettyTree(myBST.root);
+console.log(myBST)
 //       10              
 //   04      20      
 // 02  08  15  30  
 
-
+//works only for perfect tree
 function printPrettyTree(node) {
 
   function genHash(node, depth){
